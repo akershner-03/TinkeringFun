d301b3dc389a1ade269a495b9c24f20b18653dcf
Alyssa Kershner <akershner@zagmail.gonzaga.edu>
2026-09-17 12:25:42 -0700

    get input from user to calculate work and power

---
+++ variables_lab.py
@@ -0,0 +1,11 @@
+force = float(input("Enter a force in newtons: "))
+distance = input("Enter a distance in meters: ")
+distance = float(distance)
+work = force * distance
+print("work:",work)
+print(type(work))
+
+time = 6120
+power = work / time
+print("power:",power)
+print(type(power))
